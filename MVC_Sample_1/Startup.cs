b64e1d8d2e31abf30223949b66312c5374480d40
idesignerse <heberandrade@gmail.com>
2021-04-14 11:46:01 +0200

    Testing Local Server
    Testing Local Server
    Testing Adding a new item

--- MVC_Sample_1/Startup.cs
+++ MVC_Sample_1/Startup.cs
@@ -24,6 +24,7 @@ namespace MVC_Sample_1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<VehicleContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
