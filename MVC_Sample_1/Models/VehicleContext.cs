67e56eb7ed1e67d5f1116a2480d3802461345fbf
idesignerse <heberandrade@gmail.com>
2021-04-14 11:11:44 +0200

    Adding a Context File
    Moved Class.css under Model map
    Created a Context file to communicate with data-base.

---
+++ MVC_Sample_1/Models/VehicleContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Sample_1
+{
+    public class VehicleContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = Vehicle; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
+
